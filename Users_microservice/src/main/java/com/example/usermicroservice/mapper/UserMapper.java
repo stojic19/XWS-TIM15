02c4542b14258007318c6727171f1b516456a80f
Aleksa Stojić <aleksa.stojic19@uns.ac.rs>
2022-05-04 16:37:10 +0200

    feat: Added update method into user controller

--- Users_microservice/src/main/java/com/example/usermicroservice/mapper/UserMapper.java
+++ Users_microservice/src/main/java/com/example/usermicroservice/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.example.usermicroservice.mapper;
 
+import com.example.usermicroservice.dto.UpdateUserDto;
 import com.example.usermicroservice.dto.UserDto;
 import com.example.usermicroservice.model.User;
 
@@ -10,5 +11,9 @@ public class UserMapper {
     }
     public UserDto UserToUserDto(User user){
         return new UserDto(user.getId(), user.getUsername(), user.getPassword(), user.getName(), user.getEmail(), user.getTelephoneNo(), user.getGender(), user.getDateOfBirth(), user.getBiography());
+    }
+
+    public User UpdateUserDtoToUser(UpdateUserDto userDto) {
+        return new User(userDto.getId(), userDto.getUsername(), userDto.getPassword(), userDto.getName(), userDto.getEmail(), userDto.getTelephoneNo(), userDto.getGender(), userDto.getDateOfBirth(), userDto.getBiography());
     }
 }
