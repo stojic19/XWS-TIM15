680859b82c70cf075c76b055a14d528325f6d32e
Milan Podunavac <milan.podunavac@uns.ac.rs>
2022-05-07 21:58:27 +0200

    feat: Incomplete service setup

--- Posts_microservice/domain/model.go
+++ Posts_microservice/domain/model.go
@@ -1,1 +1,28 @@
 package domain
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	Id string
+}
+
+type Post struct {
+	Id         primitive.ObjectID
+	Title      string
+	Content    string
+	CreateTime time.Time
+	Owner      User
+	Comments   []Comment
+	Likes      []User
+	Dislikes   []User
+}
+
+type Comment struct {
+	Id       primitive.ObjectID
+	Owner    User
+	Content  string
+	Comments []Comment
+}
--- Posts_microservice/infrastructure/api/posts_grpc_api.go
+++ Posts_microservice/infrastructure/api/posts_grpc_api.go
@@ -1,9 +1,14 @@
 package api
 
-import "github.com/stojic19/XWS-TIM15/posts_microservice/application"
+import (
+	"context"
+	"github.com/stojic19/XWS-TIM15/common/proto/posts"
+	"github.com/stojic19/XWS-TIM15/posts_microservice/application"
+	"github.com/stojic19/XWS-TIM15/posts_microservice/domain"
+)
 
 type PostsHandler struct {
-	//unimplementedservice
+	posts.UnimplementedPostsServiceServer
 	service *application.PostsService
 }
 
@@ -11,4 +16,34 @@ func NewPostsHandler(service *application.PostsService) *PostsHandler {
 	return &PostsHandler{
 		service: service,
 	}
+}
+
+func (handler *PostsHandler) GetAll(ctx context.Context, request *posts.GetAllRequest) (*posts.GetAllResponse, error) {
+	products, err := handler.service.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	response := &posts.GetAllResponse{
+		Posts: []*posts.Post{},
+	}
+	for _, product := range products {
+		current := mapProduct(product)
+		response.Posts = append(response.Post, current)
+	}
+	return response, nil
+}
+
+func mapProduct(post *domain.Post) *posts.Post {
+	postPb := &pb.Product{
+		Id:            post.Id.Hex(),
+		Name:          post.Name,
+		ClothingBrand: post.ClothingBrand,
+	}
+	for _, color := range post.Colors {
+		postPb.Colors = append(postPb.Colors, &pb.Color{
+			Code: color.Code,
+			Name: color.Name,
+		})
+	}
+	return postPb
 }
