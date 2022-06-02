a51a045d2d75deab3efc535f806662e2ee3f8dca
Aleksa Stojić <aleksa.stojic19@uns.ac.rs>
2022-06-02 22:51:03 +0200

    feat: added PostCard, PostList and component which shows posts by user id

---
+++ dislinkt-frontend/src/components/PostsByUserId.js
@@ -0,0 +1,42 @@
+import { useParams } from "react-router-dom";
+import PostList from "./PostList";
+import { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+
+import axios from 'axios';
+
+const PostsByUserId = () => {
+    const { id } = useParams();
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            axios.get(axios.defaults.baseURL + 'posts/postsFromUser/' + id)
+                .then(res => {
+                    console.log(res.data.posts);
+                    let posts = Array.from(res.data.posts)
+                    setPosts(posts);
+                    setLoading(false);
+                }).catch(err => {
+                    console.log(err);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: err.data,
+                    });
+                });
+        };
+        fetchPosts();
+    }, []);
+
+    return (
+        <div>
+            {loading && <h3>Loading...</h3>}
+            {!loading && posts && <PostList posts={posts} />}
+        </div>
+    );
+}
+
+export default PostsByUserId;
--- dislinkt-frontend/src/components/Registration.js
+++ dislinkt-frontend/src/components/Registration.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
