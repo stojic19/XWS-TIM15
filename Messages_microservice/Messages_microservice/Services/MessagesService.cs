af3cc9dedd53696ce84c297624bef3eecefbf3ca
Aleksandar Radišić <aleksandar.radisic@uns.ac.rs>
2022-06-16 22:49:45 +0200

    Poc
    Gateway connected to dotnet grpc microservice

---
+++ Messages_microservice/Messages_microservice/Services/MessagesService.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Messages_microservice.Protos;
+
+namespace Messages_microservice.Services
+{
+    public class MessagesService : Messages.MessagesBase
+    {
+        public override Task<GetResponse> Get(GetRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GetResponse
+            {
+                Message = "Alo"
+            });
+        }
+    }
+}
