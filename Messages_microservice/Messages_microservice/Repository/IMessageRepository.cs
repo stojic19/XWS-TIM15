13632d7675cd2eebe5a80f08039957fc3bdc2f9a
Aleksandar Radišić <aleksandar.radisic@uns.ac.rs>
2022-06-18 18:42:07 +0200

    mongo init

---
+++ Messages_microservice/Messages_microservice/Repository/IMessageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Messages_microservice.model;
+
+namespace Messages_microservice.Repository
+{
+    public interface IMessageRepository
+    {
+        public IEnumerable<Message> GetAll();
+        public Message Add(Message message);
+    }
+}
---
+++ Messages_microservice/Messages_microservice/Repository/MessageRepository.cs
@@ -0,0 +1,30 @@
+﻿using Messages_microservice.model;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Messages_microservice.Repository
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly IMongoCollection<Message> _messages;
+
+        public MessageRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var mongoDatabase = mongoClient.GetDatabase("messages");
+            _messages = mongoDatabase.GetCollection<Message>("messages");
+        }
+
+        public IEnumerable<Message> GetAll()
+        { 
+            return _messages.Find(_ => true).ToList();
+        }
+
+        public Message Add(Message message)
+        { 
+            _messages.InsertOne(message);
+            return message;
+        }
+    }
+}
